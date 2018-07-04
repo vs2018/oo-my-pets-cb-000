9f1103b4f7f08ced3e4ecc195cf09990cf68501e
Vishal Sharma <vsh1819@gmail.com>
2018-07-04 21:05:57 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,10 +3,12 @@ class Owner
   attr_accessor :pets
   
   @@all = []
+  @@count = 0
   
   def initialize(type)
     @pets = {fishes: [], cats: [], dogs: []}
     @@all << self if type == "human"
+    @@count += 1 if type =="human"
   end
   
   def self.all
