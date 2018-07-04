0cc84769bedc3f495829773200b3514cc641641a
Vishal Sharma <vsh1819@gmail.com>
2018-07-04 21:13:45 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -6,7 +6,7 @@ class Owner
   @@count = 0
   
   def initialize(type)
-    @pets = {fishes: [ ], cats: [ ], dogs: [ ]}
+    @pets = {fishes: [], cats: [], dogs: []}
     @@all << self if type == "human"
     @@count += 1 if type =="human"
   end
@@ -33,7 +33,7 @@ class Owner
   
   def buy_fish(name)
     new_fish = Fish.new(name)
-    @pets["fishes"] << (new_fish)
+    @pets.fishes << (new_fish)
   end
   
 end
